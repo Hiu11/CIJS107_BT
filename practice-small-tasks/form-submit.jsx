865f03904d678d8579a0d74d8e45daa107e9f79d
Hiu11 <Tronghieudo2k4@gmail.com>
2025-10-27 21:20:00 +0700

    practice: add form submit exercise

---
+++ practice-small-tasks/form-submit.jsx
@@ -0,0 +1,8 @@
+﻿export default function SimpleForm() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("submitted");
+  }
+
+  return <form onSubmit={handleSubmit}><button>Save</button></form>;
+}
