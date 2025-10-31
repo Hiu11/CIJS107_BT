dd1a73367ade789c7bee7c6adf17bef400b1e2cc
Hiu11 <Tronghieudo2k4@gmail.com>
2025-10-31 19:44:00 +0700

    practice: add filter search exercise

---
+++ practice-small-tasks/filter-search.js
@@ -0,0 +1,5 @@
+﻿function searchItems(items, keyword) {
+  return items.filter((item) => item.toLowerCase().includes(keyword.toLowerCase()));
+}
+
+console.log(searchItems(["React", "Router", "State"], "ro"));
