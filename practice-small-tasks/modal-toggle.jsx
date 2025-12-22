0e27a97f786936795f95bc8d79ff139d2d54dc37
Hiu11 <Tronghieudo2k4@gmail.com>
2025-12-22 19:26:00 +0700

    practice: add modal toggle exercise

---
+++ practice-small-tasks/modal-toggle.jsx
@@ -0,0 +1,6 @@
+﻿import { useState } from "react";
+
+export default function ModalToggle() {
+  const [open, setOpen] = useState(false);
+  return <button onClick={() => setOpen(!open)}>{open ? "Close" : "Open"}</button>;
+}
